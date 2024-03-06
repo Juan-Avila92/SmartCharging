c756bfe52593b9c15817549f4148c356f029f63f
Juan Avila <jc.avila1092@gmail.com>
2024-03-05 21:19:22 -0500

    adds new SmartCharging.Domain folder. It contains new Domain classes such as: SmartGroup and ChargeStation

---
+++ SmartCharging.Domain/Models/ChargeStation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCharging.Domain.Models
+{
+    public class ChargeStation
+    {
+        public ChargeStation()
+        {
+            Name = string.Empty;
+        }  
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
