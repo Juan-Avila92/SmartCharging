ee682c577249dfc4d603637427a2eafaf084360b
Juan Avila <jc.avila1092@gmail.com>
2024-03-07 18:35:20 -0500

    adds minor adjustments in class naming

---
+++ SmartCharging/Data/SmartChargingDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartCharging.API.Domain.Models;
+
+namespace SmartCharging.API.Data
+{
+    public class SmartChargingDbContext : DbContext
+    {
+        public SmartChargingDbContext(DbContextOptions<SmartChargingDbContext> options) : base(options)
+        {
+        }
+        public DbSet<SmartGroup> SmartGroups { get; set; }
+        public DbSet<ChargeStation> ChargeStations { get; set; }
+    }
+}
