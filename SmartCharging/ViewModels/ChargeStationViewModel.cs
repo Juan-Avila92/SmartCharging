4cce59c633d918eae40349e830983c19608cf5e1
Juan Avila <jc.avila1092@gmail.com>
2024-03-07 18:38:16 -0500

    adds view model classes.

---
+++ SmartCharging/ViewModels/ChargeStationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartCharging.API.ViewModels
+{
+    public class ChargeStationViewModel
+    {
+        public ChargeStationViewModel()
+        {
+            Name = string.Empty;
+        }
+
+        public Guid ChargeStationId { get; set; }
+        public Guid SmartGroupId { get; set; }
+
+        public string Name { get; set; }
+
+        public int Connectors { get; set; }
+    }
+}
---
+++ SmartCharging/ViewModels/SmartGroupViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartCharging.API.ViewModels
+{
+    public class SmartGroupViewModel
+    {
+        public SmartGroupViewModel()
+        {
+            Name = string.Empty;
+            CapacityInAmps = 0;
+        }
+
+        public Guid SmartGroupId { get; set; }
+
+        public string Name { get; set; }
+
+        public int CapacityInAmps { get; set; }
+    }
+}
