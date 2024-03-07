2b6bd950a78eb2f207252d1e648b895af9602e37
Juan Avila <jc.avila1092@gmail.com>
2024-03-07 18:39:04 -0500

    adds DTOs

---
+++ SmartCharging/DTOs/ChargeStationDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace SmartCharging.API.DTOs
+{
+    public class ChargeStationDTO
+    {
+        public ChargeStationDTO() 
+        { 
+            Name = string.Empty;
+            Connectors = 1;
+        }
+        public string Name { get; set; }
+        public int Connectors { get; set; }
+    }
+}
---
+++ SmartCharging/DTOs/SmartGroupDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace SmartCharging.API.DTOs
+{
+    public class SmartGroupDTO
+    {
+        public SmartGroupDTO() 
+        { 
+            Name = string.Empty;
+            
+        }
+        public string Name { get; set; }
+        public int CapacityInAmps { get; set; }
+    }
+}
