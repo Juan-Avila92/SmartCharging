46a2f64d1af0c2db541f4aaae446877df4eb4a37
Juan Avila <jc.avila1092@gmail.com>
2024-03-07 18:37:33 -0500

    adds static class to handle numerical constants.

---
+++ SmartCharging/Domain/Utilites/RangeValues.cs
@@ -0,0 +1,20 @@
+﻿namespace SmartCharging.API.Domain.Utilities
+{
+    public static class RangeValues
+    {
+        public static int GetMinumumCapacityInAmps()
+        {
+            return 0;
+        }
+
+        public static int GetMinumumNumberOfConnectors()
+        {
+            return 1;
+        }
+
+        public static int GetMaximumNumberOfConnectors()
+        {
+            return 5;
+        }
+    }
+}
