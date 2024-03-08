d85d188391ce6ccfe2b3b7c37a8c08e2d45fe96d
Juan Avila <jc.avila1092@gmail.com>
2024-03-08 15:09:50 -0500

    Adds new Result DTO to get responses from different services.

---
+++ SmartCharging/DTOs/Result.cs
@@ -0,0 +1,22 @@
+﻿namespace SmartCharging.API.DTOs
+{
+    public class Result
+    {
+        public string Message { get; protected set; }
+        public bool IsOk { get; protected set; }
+
+        public Result Ok(string message = null) 
+        { 
+            Message = string.IsNullOrEmpty(message) ? string.Empty : message;
+            IsOk = true;
+            return this;
+        }
+
+        public Result Fail(string message = null)
+        {
+            Message = string.IsNullOrEmpty(message) ? string.Empty : message;
+            IsOk = false;
+            return this;
+        }
+    }
+}
---
+++ SmartCharging/DTOs/Result[TData].cs
@@ -0,0 +1,24 @@
+﻿namespace SmartCharging.API.DTOs
+{
+    public class Result<TData> : Result where TData : class
+    {
+        public TData Data { get; private set; }
+        public Result<TData> WithData(TData data)
+        {
+            Data = data;
+            return this;
+        }
+
+        public new Result<TData> Ok(string message = null)
+        {
+            base.Ok(message);
+            return this;
+        }
+
+        public new Result<TData> Fail(string message = null)
+        {
+            base.Fail(message);
+            return this;
+        }
+    }
+}
